3d0236d616768ca663415ec3ccf53479b51e226e
e <baichenxu3@qq.com>
2021-02-05 15:43:56 +0800

    modify cli package version

---
+++ multiple-languages/javascript/alicloud-ros-cdk-cli-1.0.5/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -0,0 +1,9 @@
+import ros_cdk_core as core
+
+
+class %name.PascalCased%Stack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
